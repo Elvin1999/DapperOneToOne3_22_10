c6547a349b4745c1dd22e4370e568bd326eab44b
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2024-02-13 10:09:52 +0400

    Add project files.

---
+++ DapperOneToOne/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using DapperOneToOne.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DapperOneToOne
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var conn = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+            using (var connection = new SqlConnection(conn))
+            {
+                var sql = @"   SELECT CAP.CapitalId,CAP.Name,CAP.CountryId,
+                                C.CountryId,C.Name
+                                FROM Capitals AS CAP
+                                INNER JOIN Countries AS C
+                                ON CAP.CountryId=C.CountryId
+";
+
+                var capitals = connection.Query<Capital, Country, Capital>(sql,
+                    (capital, country) =>
+                    {
+                        capital.Country = country;
+                        return capital;
+                    }, splitOn: "CountryId");
+
+                myDataGrid.ItemsSource = capitals;
+            }
+        }
+    }
+}
---
+++ DapperOneToOne/SQLQuery1.sql
@@ -0,0 +1,26 @@
+﻿CREATE DATABASE OneToOneDB
+GO
+USE OneToOneDB
+GO
+
+CREATE TABLE Countries(
+CountryId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Name] NVARCHAR(30) NOT NULL
+)
+
+GO
+INSERT INTO Countries([Name])
+VALUES('Norway'),('Canada'),('Bahamas')
+
+GO
+
+CREATE TABLE Capitals(
+CapitalId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Name] NVARCHAR(30) NOT NULL,
+CountryId INT FOREIGN KEY REFERENCES Countries(CountryId),
+UNIQUE(CountryId)
+)
+GO
+
+INSERT INTO Capitals([Name],[CountryId])
+VALUES('Oslo',1),('Ottawa',2),('Nassau',3)
